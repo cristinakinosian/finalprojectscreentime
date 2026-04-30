2738456636fedabe11bfe15a9986d11588c55d07
cristinakinosian <ckinosia@slcc.edu>
2026-04-29 22:01:09 -0600

    Update bar graph colors in show_bar_graph function

--- myapp.py
+++ myapp.py
@@ -29,7 +29,7 @@ NEGATIVE_THRESHOLD = 2         # 2 hours recreational screen time
 
 # graph display functions
 
-def show_bar_graph(labels, values, title, ylabel="Hours per day", colors=['#801650','#E9724D', '#A6D0AE']):
+def show_bar_graph(labels, values, title, ylabel="Hours per day", colors=['#801650','#7D98B5', '#FFE68F']):
     """display streamlit-compatible bar graph."""
     fig, ax = plt.subplots(figsize=(4, 4))
     ax.bar(labels, values, color=colors)
